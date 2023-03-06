70c4b0a43e58bcaffdf5b1eae2c584f79171aed5
Guillaume Mali <guillaume.mali@student.hepl.be>
2023-03-06 12:12:25 +0100

    work on ruche

--- lib/models/ruche_model.dart
+++ lib/models/ruche_model.dart
@@ -26,6 +26,7 @@ class Ruche {
       deployment: json['deployment'],
       idRucher: json['idRucher'],
       idProprietaire: json['idProprietaire']);
+  
 
   Map<String, dynamic> toJson() => {
         'hiveid': hiveid,
